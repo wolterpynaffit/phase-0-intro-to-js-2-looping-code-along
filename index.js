c8478dbdaf4d9839e6019aaf6c0355f39099d72a
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-05-13 13:06:48 -0400

    loops

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"]
+const event = surprise
+
+function  writeCards(names, event){
+    var newArr = []
+    for (let i=0; i < names.length; i++){
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr
+}
+
+
+
+let count = 10;
+
+function countDown(count){
+    if (count % 2 === 0){
+
+        while (count >= 0) {
+            console.log(count--)
+          }
+    }
+}
+
+countDown(count)
